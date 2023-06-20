edd6dc9737b93a72ebf4a597be4169645e80b1bd
ns009 <65356200+ns009@users.noreply.github.com>
2023-06-20 13:48:42 +0800

    Update editports.sh

--- editports.sh
+++ editports.sh
@@ -5,10 +5,7 @@ echo -e "\e[93m            [1] Edit Port Dropbear"
 echo -e "\e[93m            [2] Edit Port Stunnel"
 echo -e "\e[93m            [x] Exit"
 echo -e "\e[0m                                                   "
-read -p "       Select From Options [1-2 or x] :  " Port
-echo -e "\e[0m                                                   "
 echo -e "\e[94m[][][]======================================[][][]\e[0m"
-case $Port
 			
 			1)
 			clear
