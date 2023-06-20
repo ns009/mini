7071dded5a3520e12a588617716e7678ea2aaeda
ns009 <65356200+ns009@users.noreply.github.com>
2023-06-20 13:59:04 +0800

    Update editports.sh

--- editports.sh
+++ editports.sh
@@ -1,24 +1,21 @@
-echo -e "\e[94m[][][]======================================[][][]"
-echo -e "\e[0m                                                   "
-echo -e "\e[93m            [1] Edit Port Dropbear"
-echo -e "\e[93m            [2] Edit Port Stunnel"
-echo -e "\e[93m            [x] Exit"
-echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]\e[0m"
-read -p "Masukkan pilihan anda, kemudian tekan ENTER: " option1
+echo -e "=====================***-MENU-***====================="
+echo -e "\e[033;1m-SSH & VPN"
+echo -e "\e[031;1m 1\e[0m) Edit Port Dropbear (\e[34;1m\e[0m)"
+echo -e "\e[031;1m 2\e[0m) Edit Port Stunnel (\e[34;1m\e[0m)"
+echo -e "=====================***-NS-SSH-***===================="
+echo -e "\e[031;1mx\e[0m) Exit"
+echo -e ""
+	read -p "Masukkan pilihan anda, kemudian tekan ENTER: " option1
 	case $option1 in
-   			1)
-			clear
-			editdropbear
-			exit
-			;;
-			2)
-			clear
-			editstunnel4
-			exit
-			;;
-			x)
-			clear
-			exit
-			;;
-	esac
+	1)  
+        clear
+        usernew
+        ;;
+        2)  
+        clear
+        deluser
+        ;;
+        x)
+        ;;
+        
+        esac
