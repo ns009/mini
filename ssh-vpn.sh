b2d62ed7f4e928618e098b9708a3acaeffda16fc
ns009 <65356200+ns009@users.noreply.github.com>
2023-06-20 12:59:07 +0800

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -11,15 +11,6 @@ NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
 source /etc/os-release
 ver=$VERSION_ID
 
-#detail nama perusahaan
-country=MY
-state=KL
-locality=MY
-organization=ratt.website
-organizationalunit=ratt.website
-commonname=ratt.website
-email=admin@ratt.website
-
 # go to root
 cd
 
