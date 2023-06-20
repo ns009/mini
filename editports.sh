c2136318183aeacfeca9cf342522cbeafff2fe03
ns009 <65356200+ns009@users.noreply.github.com>
2023-06-20 14:18:18 +0800

    Update editports.sh

--- editports.sh
+++ editports.sh
@@ -3,8 +3,8 @@ echo -e "=====================***-MENU-***====================="
 echo -e "\e[033;1m-SSH & VPN"
 echo -e "\e[031;1m 1\e[0m) Edit Port Dropbear (\e[34;1m\e[0m)"
 echo -e "\e[031;1m 2\e[0m) Edit Port Stunnel (\e[34;1m\e[0m)"
-echo -e "=====================***-NS-SSH-***===================="
 echo -e "\e[031;1mx\e[0m) Exit"
+echo -e "=====================***-NS-SSH-***===================="
 echo -e ""
 	read -p "Masukkan pilihan anda, kemudian tekan ENTER: " option1
 	case $option1 in
