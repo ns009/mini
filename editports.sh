5361485d448801e5fe6ad896012795b28c191273
ns009 <65356200+ns009@users.noreply.github.com>
2023-06-20 14:29:21 +0800

    Update editports.sh

--- editports.sh
+++ editports.sh
@@ -2,8 +2,8 @@
 
 echo -e "=====================***-MENU-***====================="
 echo -e "\e[033;1m-SSH & VPN"
-echo -e "\e[031;1m 1\e[0m) Edit Port Dropbear (\e[34;1m\e[0m)"
-echo -e "\e[031;1m 2\e[0m) Edit Port Stunnel (\e[34;1m\e[0m)"
+echo -e "\e[031;1m 1\e[0m) Edit Port Dropbear"
+echo -e "\e[031;1m 2\e[0m) Edit Port Stunnel"
 echo -e "\e[031;1mx\e[0m) Exit"
 echo -e "=====================***-NS-SSH-***===================="
 echo -e ""
