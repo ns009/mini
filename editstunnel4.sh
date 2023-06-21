6480922f7e06d03e874c83522294a23bc54b8c7a
ns009 <65356200+ns009@users.noreply.github.com>
2023-06-21 09:05:06 +0800

    Update editstunnel4.sh

--- editstunnel4.sh
+++ editstunnel4.sh
@@ -1,60 +1,97 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- https://icanhazip.com);
-echo "Checking VPS"
+rm -f /root/opensshport
+rm -f /root/dropbearport
+rm -f /root/stunnel4port
+rm -f /root/openvpnport
+rm -f /root/squidport
+opensshport="$(netstat -ntlp | grep -i ssh | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+dropbearport="$(netstat -nlpt | grep -i dropbear | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+stunnel4port="$(netstat -nlpt | grep -i stunnel | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+openvpnport="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+squidport="$(cat /etc/squid3/squid.conf | grep -i http_port | awk '{print $2}')"
+echo $opensshport > /root/opensshport
+cat > /root/opensshport <<-END
+$opensshport
+END
+echo $dropbearport > /root/dropbearport
+cat > /root/dropbearport <<-END
+$dropbearport
+END
+echo $stunnel4port > /root/stunnel4port
+cat > /root/stunnel4port <<-END
+$stunnel4port
+END
+echo $openvpnport > /root/openvpnport
+cat > /root/openvpnport <<-END
+$openvpnport
+END
+echo $squidport > /root/squidport
+cat > /root/squidport <<-END
+$squidport
+END
 
+cd
 clear
-ssl="$(cat /etc/stunnel/stunnel.conf | grep -i accept | head -n 2 | cut -d= -f2 | sed 's/ //g' | tr '\n' ' ' | awk '{print $1}')"
-ssl2="$(cat /etc/stunnel/stunnel.conf | grep -i accept | head -n 2 | cut -d= -f2 | sed 's/ //g' | tr '\n' ' ' | awk '{print $2}')"
-echo -e "======================================"
-echo -e ""
-echo -e "     [1]  Change Port $ssl"
-echo -e "     [2]  Change Port $ssl2"
-echo -e "     [x]  Exit"
-echo -e "======================================"
-echo -e ""
-read -p "     Select From Options [1-2 or x] :  " prot
-echo -e ""
-case $prot in
-1)
-read -p "New Port Stunnel4: " stl
-if [ -z $stl ]; then
-echo "Please Input Port"
-exit 0
-fi
-cek=$(netstat -nutlp | grep -w $stl)
-if [[ -z $cek ]]; then
-sed -i "s/$ssl/$stl/g" /etc/stunnel/stunnel.conf
-sed -i "s/   - Stunnel4                : $ssl, $ssl2/   - Stunnel4                : $stl, $ssl2/g" /root/log-install.txt
-/etc/init.d/stunnel4 restart > /dev/null
-echo -e "\e[032;1mPort $stl modified successfully\e[0m"
+echo -e "\e[0m                                                   "
+echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[0m                                                   "
+echo -e "\e[93m                  Stunnel4  Port                  "
+echo -e "\e[93m                        "$stunnel4port
+echo -e "\e[0m                                                   "
+read -p "       Which Port Should Be Changed? :  " Port
+egrep "^$Port" /root/stunnel4port >/dev/null
+if [ $? -eq 0 ]; then
+	read -p "            From Port $Port -> Port " Port_New
+	if grep -Fxq $Port_New /root/opensshport; then
+		echo -e "\e[0m                                                   "
+		echo -e "\e[91m              OpenSSH Port Conflict              "
+		echo -e "\e[91m              Port Is Already In Use              "
+		echo -e "\e[0m                                                   "
+		echo -e "\e[94m[][][]======================================[][][]\e[0m"
+		exit
+	fi
+	if grep -Fxq $Port_New /root/dropbearport; then
+		echo -e "\e[0m                                                   "
+		echo -e "\e[91m              Dropbear Port Conflict              "
+		echo -e "\e[91m              Port Is Already In Use              "
+		echo -e "\e[0m                                                   "
+		echo -e "\e[94m[][][]======================================[][][]\e[0m"
+		exit
+	fi
+	if grep -Fxq $Port_New /root/openvpnport; then
+		echo -e "\e[0m                                                   "
+		echo -e "\e[91m              Openvpn Port Conflict               "
+		echo -e "\e[91m              Port Is Already In Use              "
+		echo -e "\e[0m                                                   "
+		echo -e "\e[94m[][][]======================================[][][]\e[0m"
+		exit
+	fi
+	if grep -Fxq $Port_New /root/squidport; then
+		echo -e "\e[0m                                                   "
+		echo -e "\e[91m               Squid3 Port Conflict               "
+		echo -e "\e[91m              Port Is Already In Use              "
+		echo -e "\e[0m                                                   "
+		echo -e "\e[94m[][][]======================================[][][]\e[0m"
+		exit
+	fi
+	Port_Change="s/$Port/$Port_New/g";
+	sed -i $Port_Change /etc/stunnel/stunnel.conf
+	service stunnel4 restart > /dev/null
+	rm -f /root/stunnel4port
+	stunnel4port="$(netstat -nlpt | grep -i stunnel | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+	clear
+	echo -e "\e[0m                                                   "
+	echo -e "\e[94m[][][]======================================[][][]"
+	echo -e "\e[0m                                                   "
+	echo -e "\e[93m                  Stunnel4  Port                  "
+	echo -e "\e[93m                        "$stunnel4port
+	echo -e "\e[0m                                                   "
+	echo -e "\e[94m[][][]======================================[][][]\e[0m"
+
 else
-echo "Port $stl is used"
+	clear
+	echo -e "\e[0m                                                   "
+	echo -e "\e[91m                 Port Doesnt Exit                 "
+	echo -e "\e[0m                                                   "
+	echo -e "\e[94m[][][]======================================[][][]\e[0m"
 fi
-;;
-2)
-read -p "New Port Stunnel4: " stl
-if [ -z $stl ]; then
-echo "Please Input Port"
-exit 0
-fi
-cek=$(netstat -nutlp | grep -w $stl)
-if [[ -z $cek ]]; then
-sed -i "s/$ssl2/$stl/g" /etc/stunnel/stunnel.conf
-sed -i "s/   - Stunnel4                : $ssl, $ssl2/   - Stunnel4                : $ssl, $stl/g" /root/log-install.txt
-/etc/init.d/stunnel4 restart > /dev/null
-echo -e "\e[032;1mPort $stl modified successfully\e[0m"
-else
-echo "Port $stl is used"
-fi
-;;
-x)
-exit
-menu
-;;
-*)
-echo "Please enter an correct number"
-;;
-esac
