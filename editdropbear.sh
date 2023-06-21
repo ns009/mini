18ea7e61a0226b9b6d86247f46d93428b895c9c7
ns009 <65356200+ns009@users.noreply.github.com>
2023-06-21 09:10:48 +0800

    Update editdropbear.sh

--- editdropbear.sh
+++ editdropbear.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-rm -f /root/opensshport
 rm -f /root/dropbearport
 rm -f /root/stunnel4port
-rm -f /root/openvpnport
-rm -f /root/squidport
-opensshport="$(netstat -ntlp | grep -i ssh | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 dropbearport="$(netstat -nlpt | grep -i dropbear | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 stunnel4port="$(netstat -nlpt | grep -i stunnel | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
-openvpnport="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
-squidport="$(cat /etc/squid3/squid.conf | grep -i http_port | awk '{print $2}')"
-echo $opensshport > /root/opensshport
-cat > /root/opensshport <<-END
-$opensshport
-END
 echo $dropbearport > /root/dropbearport
 cat > /root/dropbearport <<-END
 $dropbearport
@@ -21,15 +11,6 @@ echo $stunnel4port > /root/stunnel4port
 cat > /root/stunnel4port <<-END
 $stunnel4port
 END
-echo $openvpnport > /root/openvpnport
-cat > /root/openvpnport <<-END
-$openvpnport
-END
-echo $squidport > /root/squidport
-cat > /root/squidport <<-END
-$squidport
-END
-
 cd
 clear
 echo -e "\e[0m                                                   "
@@ -42,33 +23,9 @@ read -p "       Which Port Should Be Changed? :  " Port
 egrep "^$Port" /root/dropbearport >/dev/null
 if [ $? -eq 0 ]; then
 	read -p "            From Port $Port -> Port " Port_New
-	if grep -Fxq $Port_New /root/opensshport; then
-		echo -e "\e[0m                                                   "
-		echo -e "\e[91m              OpenSSH Port Conflict              "
-		echo -e "\e[91m              Port Is Already In Use              "
-		echo -e "\e[0m                                                   "
-		echo -e "\e[94m[][][]======================================[][][]\e[0m"
-		exit
-	fi
 	if grep -Fxq $Port_New /root/stunnel4port; then
 		echo -e "\e[0m                                                   "
 		echo -e "\e[91m              Stunnel4 Port Conflict              "
-		echo -e "\e[91m              Port Is Already In Use              "
-		echo -e "\e[0m                                                   "
-		echo -e "\e[94m[][][]======================================[][][]\e[0m"
-		exit
-	fi
-	if grep -Fxq $Port_New /root/openvpnport; then
-		echo -e "\e[0m                                                   "
-		echo -e "\e[91m              Openvpn Port Conflict               "
-		echo -e "\e[91m              Port Is Already In Use              "
-		echo -e "\e[0m                                                   "
-		echo -e "\e[94m[][][]======================================[][][]\e[0m"
-		exit
-	fi
-	if grep -Fxq $Port_New /root/squidport; then
-		echo -e "\e[0m                                                   "
-		echo -e "\e[91m               Squid3 Port Conflict               "
 		echo -e "\e[91m              Port Is Already In Use              "
 		echo -e "\e[0m                                                   "
 		echo -e "\e[94m[][][]======================================[][][]\e[0m"
